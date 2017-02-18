651f73e543ad49d5cb46f004a2bfc8d9be56f7d8
Jonathan Goode <u01jmg3@gmail.com>
2017-02-18 14:02:33 +0000

    Tabs to spaces

--- notify.php
+++ notify.php
@@ -1,18 +1,18 @@
 <?php
-	$dirname = 'C:\\Users\\[change me]\\Desktop\\';
-	$filename = (isset($_POST['filename']) ? preg_replace('/[\\|\/|\:|\*|\?|\"|\<|\>|\|]/', '', $_POST['filename']) : 'notify') . '.txt';
-	
-	if(isset($_POST['save_file']) && $_POST['save_file'] == '1'){
-		if(is_dir($dirname)){
-			$path = $dirname . $filename;
-			
-			if(!file_exists($path)){
-				$handle = fopen($path, 'w');
-				fclose($handle);
-				
-				if(file_exists($path))
-					echo 1;
-			}
-		}
-	} else if(isset($_POST['save_file']) && $_POST['save_file'] == '0')
-		echo 1;
+    $dirname = 'C:\\Users\\[change me]\\Desktop\\';
+    $filename = (isset($_POST['filename']) ? preg_replace('/[\\|\/|\:|\*|\?|\"|\<|\>|\|]/', '', $_POST['filename']) : 'notify') . '.txt';
+    
+    if(isset($_POST['save_file']) && $_POST['save_file'] == '1'){
+        if(is_dir($dirname)){
+            $path = $dirname . $filename;
+            
+            if(!file_exists($path)){
+                $handle = fopen($path, 'w');
+                fclose($handle);
+                
+                if(file_exists($path))
+                    echo 1;
+            }
+        }
+    } else if(isset($_POST['save_file']) && $_POST['save_file'] == '0')
+        echo 1;
